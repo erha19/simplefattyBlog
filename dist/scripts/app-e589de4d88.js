926daddc2b36cda25a18dc3582fabdf1ff118699
simplefatty <simplefatty@rippletec.net>
2016-02-18 20:27:20 +0800

    build

--- dist/scripts/app-e589de4d88.js
+++ dist/scripts/app-e589de4d88.js
@@ -585,7 +585,8 @@ angular.module('sf_blog.article')
  return angular.module('sf_blog')
 .constant('ServerUrl', "http://api.jackhu.top")
 .constant('IsDebug', false)
-.constant('CookieConfig', {"domain":".jackhu.top"});
+.constant('CookieConfig', {"domain":".jackhu.top"})
+.constant('EVENT', {"NeedToLoad":"need-to-load"});
 
 })();
 
--- src/app/app.conf.js
+++ src/app/app.conf.js
@@ -2,6 +2,7 @@
  return angular.module('sf_blog')
 .constant('ServerUrl', "http://api.jackhu.top")
 .constant('IsDebug', false)
-.constant('CookieConfig', {"domain":".jackhu.top"});
+.constant('CookieConfig', {"domain":".jackhu.top"})
+.constant('EVENT', {"NeedToLoad":"need-to-load"});
 
 })();
