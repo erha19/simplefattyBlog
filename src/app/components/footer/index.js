4510b11219f855498b7c08620e6a5526c9e58b9a
simplefatty <simplefatty@rippletec.net>
2016-03-05 12:13:49 +0800

    build 20160305

--- src/app/components/footer/index.js
+++ src/app/components/footer/index.js
@@ -2,15 +2,20 @@
 	'use strict';
 
 	angular.module('sf_blog').component('sfFooter', {
-		templateUrl: 'app/components/footer/footer.html',
-		controller: 'footerCtrl',
-		controllerAs: 'footer'
-		// ,
-		// bindings: {
-		// }
-	})
-	.controller('footerCtrl', function() {
-	  this.info = 'Simplefatty © 2015-2016 ';
-	});
+			templateUrl: 'app/components/footer/footer.html',
+			controller: 'footerCtrl',
+			controllerAs: 'footer'
+				// ,
+				// bindings: {
+				// }
+		})
+		.controller('footerCtrl', ['$rootScope', '$window', function() {
+			this.info = 'Simplefatty © 2015-2016 ';
+
+			$rootScope.$on('$stateChangeSuccess', function( /*event, toState, toParams, fromState, fromParams*/ ) {
+				$window.scrollTo(0, 0);
+			});
+
+		}]);
 
 })();
